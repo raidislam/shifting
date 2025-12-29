46acf44c930376b29db9bc497828352fed5246e1
raidislam <raidislam@gmail.com>
2025-12-29 18:34:49 +0600

    update

--- client/shifting/src/component/auth/login/Login.jsx
+++ client/shifting/src/component/auth/login/Login.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import { useForm } from "react-hook-form";
 
 export default function LoginComponent() {
+    const { register, handleSubmit ,formState:{errors}} = useForm();
+
+    const loginSubmit = function(data){
+        console.log(data);
+    }
+
     return (
-        <div>this login</div>
+        <form onSubmit={handleSubmit(loginSubmit)}>
+            <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input type="email" className="input" placeholder="Email" {...register('email')} />
+                <label className="label">Password</label>
+                <input type="password" className="input" placeholder="Password" {...register('password',{
+                    required: true,
+                    minLength: 6
+                })} />
+                <div><a className="link link-hover">Forgot password?</a></div>
+                <button className="btn btn-neutral mt-4">Login</button>
+            </fieldset>
+        </form>
     )
 }
--- client/shifting/src/layouts/AuthLayouts.jsx
+++ client/shifting/src/layouts/AuthLayouts.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { Outlet } from 'react-router'
 
 export default function AuthLayouts() {
-  return (
-    <div className="hero bg-base-200 min-h-screen">
+    return (
+        <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img
                     src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
@@ -14,5 +14,5 @@ export default function AuthLayouts() {
                 </div>
             </div>
         </div>
-  )
+    )
 }
